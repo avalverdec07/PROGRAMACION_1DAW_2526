609904408a3d43a8a4d503a7ed6f0b4064d7e7e2
Andrés Valverde Consentino <andresvalverde@educarex.es>
2025-09-25 11:14:48 +0200

    Actividades con variables

--- src/unidad1/holaMundo.java
+++ src/unidad1/holaMundo.java
@@ -1,9 +1,11 @@
+package unidad1;
 
 public class holaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!!!");
+		gikjd
 	}
 
 }
